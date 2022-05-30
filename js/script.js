5302cb18264794a07a4347944802cd0bdddbd055
AHMED NABIB <nabibpallab@gmail.com>
2022-05-30 23:49:04 +0600

    storage cost and delivery cost complete

--- js/script.js
+++ js/script.js
@@ -24,30 +24,39 @@ document
 
 		// total.innerText = totalPrice;
 
-        updatePrice('memory-cost', 300)
-    });
+		updatePrice("memory-cost", 300);
+	});
 
+document.getElementById("eightGB").addEventListener("click", function (params) {
+	updatePrice("memory-cost", 0);
+});
 
-    document.getElementById("ssd3").addEventListener('click', function (params) {
-        updatePrice('storage-cost',500)
-    })
+document.getElementById("ssd3").addEventListener("click", function (params) {
+	updatePrice("storage-cost", 500);
+});
 
-    documrnt
-    
+document.getElementById("ssd3").addEventListener("click", function (params) {
+	updatePrice("storage-cost", 500);
+});
 
-    function updatePrice(itemId, price) {
-		const memoryCost = document.getElementById(itemId);
-		memoryCost.innerText = price;
-		const bestPrice = document.getElementById("best-price").innerText;
-		const memoryPrice = document.getElementById("memory-cost").innerText;
-		const storagePrice = document.getElementById("storage-cost").innerText;
-		const deliveryPrice =
-			document.getElementById("delivery-cost").innerText;
-		const totalPrice =
-			parseFloat(bestPrice) +
-			parseFloat(memoryPrice) +
-			parseFloat(storagePrice) +
-			parseFloat(deliveryPrice);
-		const total = document.getElementById("total-price");
-		total.innerText = totalPrice;
-	}
+documrnt
+	.getElementById("paid-delivery")
+	.addEventListener("click", function (params) {
+		updatePrice("delivery-cost", 20);
+	});
+
+function updatePrice(itemId, price) {
+	const memoryCost = document.getElementById(itemId);
+	memoryCost.innerText = price;
+	const bestPrice = document.getElementById("best-price").innerText;
+	const memoryPrice = document.getElementById("memory-cost").innerText;
+	const storagePrice = document.getElementById("storage-cost").innerText;
+	const deliveryPrice = document.getElementById("delivery-cost").innerText;
+	const totalPrice =
+		parseFloat(bestPrice) +
+		parseFloat(memoryPrice) +
+		parseFloat(storagePrice) +
+		parseFloat(deliveryPrice);
+	const total = document.getElementById("total-price");
+	total.innerText = totalPrice;
+}
